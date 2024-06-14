11413e1a04b3092267cfd43bf812576e61a887e6
Igor Galushka <igorgalushka91@gmail.com>
2024-06-14 19:37:33 +0300

    Task 2 и Task 9 - done

--- HW1/homeTask2.php
+++ HW1/homeTask2.php
@@ -17,3 +17,47 @@ function solveTask2($a, $b): int
 
 $result = solveTask2(3, 3);
 echo $result;
+
+class Task2
+{
+    private ?int $value1 = null;
+    private ?int $value2 = null;
+
+    public function __construct($value1 = 5, $value2 = 3)
+    {
+        $this->value1 = $value1;
+        $this->value2 = $value2;
+    }
+
+    public function changeValue1(int $value)
+    {
+        $this->value1 = $value;
+
+    }
+
+    public function changeValue2(int $value)
+    {
+        $this->value2 = $value;
+
+    }
+
+    public function info()
+    {
+        echo 'Value1: ' . $this->value1 . '<br>';
+        echo 'Value2: ' . $this->value2 . '<br>';
+    }
+
+    public function solveTask2(): int
+        /*Пользователь вводит два числа.
+        *Если они не равны 10 и первое число четное,
+        *то вывести их сумму, иначе вывести их произведение.
+        */
+    {
+        if (($this->value1 !== 10) && ($this->value2 !== 10) && ($this->value1 % 2 == 0)) {
+            return $this->value1 + $this->value2;
+        } else {
+            return $this->value1 * $this->value2;
+        }
+    }
+}
+
--- HW1/homeTask9.php
+++ HW1/homeTask9.php
@@ -1,43 +1,117 @@
 <?php
 // Генерирует двухмерный массив n×m элементов
-function genMatrix(int $n = 5, int $m = 5): array {
-    $a = [];
-    for ($i = 0; $i < $n; $i++) {
-        $row = [];
-        for ($j = 0; $j < $m; $j++) {
-            $row[] = mt_rand(0, 9);
-        }
-        $a[] = $row;
-    }
-    return $a;
-}
+//function genMatrix(int $n = 5, int $m = 5): array {
+//    $a = [];
+//    for ($i = 0; $i < $n; $i++) {
+//        $row = [];
+//        for ($j = 0; $j < $m; $j++) {
+//            $row[] = mt_rand(0, 9);
+//        }
+//        $a[] = $row;
+//    }
+//    return $a;
+//}
+//
+//// Выводит матрицу в виде таблицы
+//function showMatrix(array $a): void {
+//    foreach ($a as $row) {
+//        foreach ($row as $value) {
+//            echo $value . ' ';
+//        }
+//        echo '<br>';
+//    }
+//}
+//
+//// Считает количество вхождений числа 7 в матрице
+//function countSevens(array $a): int {
+//    $count = 0;
+//    foreach ($a as $row) {
+//        foreach ($row as $value) {
+//            if ($value == 7) {
+//                $count++;
+//            }
+//        }
+//    }
+//    return $count;
+//}
+//
+//// Пример использования
+//$a = genMatrix();
+//showMatrix($a);
+//
+//echo '<br>Количество вхождений числа 7: ' . countSevens($a);
+//
 
-// Выводит матрицу в виде таблицы
-function showMatrix(array $a): void {
-    foreach ($a as $row) {
-        foreach ($row as $value) {
-            echo $value . ' ';
-        }
-        echo '<br>';
-    }
-}
 
-// Считает количество вхождений числа 7 в матрице
-function countSevens(array $a): int {
-    $count = 0;
-    foreach ($a as $row) {
-        foreach ($row as $value) {
-            if ($value == 7) {
-                $count++;
+class Task9
+{
+    private ?int $row = null;
+    private ?int $column = null;
+    private int $counter = 0;
+    private array $matrix = [];
+
+    public function __construct(int $row, int $column)
+    {
+        $this->row = $row;
+        $this->column = $column;
+        $this->matrix = $this->genMatrix();
+        $this->getmatrixInfo();
+    }
+
+    public function changeRows(int $row)
+    {
+        $this->row = $row;
+    }
+
+    public function changeColumns(int $column)
+    {
+        $this->column = $column;
+    }
+
+    private function genMatrix(): array
+    {
+        $array = [];
+        for ($i = 0; $i < $this->row; $i++) {
+            $row = [];
+            for ($j = 0; $j < $this->column; $j++) {
+                $row[] = mt_rand(0, 9);
+            }
+            $array[] = $row;
+        }
+        return $array;
+    }
+
+    private function getmatrixInfo()
+    {
+        foreach ($this->matrix as $row) {
+            foreach ($row as $value) {
+                echo $value . ' ';
+            }
+            echo '<br>';
+        }
+    }
+
+    public function countSevens()
+    {
+        foreach ($this->matrix as $row) {
+            foreach ($row as $value) {
+                if ($value === 7) {
+                    $this->counter++;
+                }
             }
         }
+        $this->showCounter();
     }
-    return $count;
+
+    private function showCounter()
+    {
+        echo "Количество цифр 7 в матрице: " . $this->counter;
+    }
 }
 
 // Пример использования
-$a = genMatrix();
-showMatrix($a);
+$matrix1 = new Task9(5, 5);
+$matrix1->countSevens();
 
-echo '<br>Количество вхождений числа 7: ' . countSevens($a);
-?>
+
+
